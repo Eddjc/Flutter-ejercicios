c3b425271bb42974f0fb2707ecd3409e8872e11e
Eddjc <eduardojose.campos@gmail.com>
2023-09-03 22:26:47 -0600

    S2/L12

---
+++ 08_getters_setters.dart
@@ -0,0 +1,36 @@
+void  main() {
+  
+  final mySquare = Square(side: 10);
+  
+  mySquare.side = -5;
+  
+  print('Area: ${ mySquare.area }');
+  
+}
+
+class Square {
+
+  double _side; // side * side
+  
+  //constructor establecemos el valor del side
+  Square({ required double side })
+    : _side = side;
+  
+  //el _ antes del nombre significa que el valor es privado eso quiere decir que solo puede ser accedido dentro de la clase Square
+  double get area {
+    return _side * _side;
+  }
+  
+  //cambiamos el valor del _side
+  set side(double value) {
+    print ('setting new value $value');
+    if (value < 0 ) throw 'valor debe de ser >= 0';
+    
+    _side = value;
+  }
+  
+  //metodo para el calculo de la area
+  double calculateArea() {
+    return _side * _side;
+  }
+}
